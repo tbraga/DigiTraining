9f236b46e4161e5a95453ebde5bfb2189f3574d7
tibraga <tibraga@cimpress.com>
2017-04-03 09:39:34 -0400

    Removing unneeded meta tags from head

--- wp-content/themes/elbrus/functions.php
+++ wp-content/themes/elbrus/functions.php
@@ -2,3 +2,15 @@
 
 	/* Include main framework file */
 	require_once( get_template_directory().'/library/loader.php');
+
+	//Remove All Meta Generators
+	function remove_meta_generators($html) {
+		$pattern = '/<meta name(.*)=(.*)"generator"(.*)>/i';
+		$html = preg_replace($pattern, '', $html);
+		return $html;
+	}
+	function clean_meta_generators($html) {
+		ob_start('remove_meta_generators');
+	}
+	add_action('get_header', 'clean_meta_generators', 100);
+	add_action('wp_footer', function(){ ob_end_flush(); }, 100);
